092fd3ba8b49eb1ec8c2e867ddcf77e6c484cb48
Coach @lfred <alfred.walther@artingo.net>
2022-02-08 19:11:39 +0000

    Log4j2 mit Konfiguration hinzugefügt

--- src/onlineshop/Main.java
+++ src/onlineshop/Main.java
@@ -1,6 +1,8 @@
 package onlineshop;
 
 import onlineshop.waren.Buch;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Diese Klasse stellt den Einstiegspunkt in das Programm dar.
@@ -13,33 +15,40 @@ import onlineshop.waren.Buch;
  * @version 1.0
  */
 public class Main {
-    /**
-     * Der verwendete Online Shop.
-     */
-    public OnlineShop onlineShop = null;
+  private static Logger LOGGER = LogManager.getLogger(Main.class);
 
-    /**
-     * Legt ein neues Buch and und benachrichtigt die
-     * Verwaltungsobjekte darüber.
-     *
-     * @param autor Name des Autors des neuen Buches
-     * @param titel Titel des neuen Buches
-     * @param hersteller Hersteller des neuen Buches
-     * @param bestand Anzahl des Buches im Lager
-     *
-     * @return neue Buch-Instanz
-     * @throws RuntimeException wenn das Aktualisieren des Bestandes fehlschlägt
-     */
-    public Buch neuesBuch(String autor, String titel,
-                          String hersteller, int bestand) {
-        Buch neuesBuch = new Buch();
-        neuesBuch.setAutor(autor);
-        neuesBuch.setTitel(titel);
-        neuesBuch.setHersteller(hersteller);
-        return neuesBuch;
-    }
+  /**
+   * Der verwendete Online Shop.
+   */
+  public OnlineShop onlineShop = null;
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    // write your code here
+    LOGGER.trace("Trace Message!");
+    LOGGER.debug("Debug Message!");
+    LOGGER.info("Info Message!");
+    LOGGER.warn("Warn Message!");
+    LOGGER.error("Error Message!");
+    LOGGER.fatal("Fatal Message!");
+  }
+
+  /**
+   * Legt ein neues Buch and und benachrichtigt die
+   * Verwaltungsobjekte darüber.
+   *
+   * @param autor      Name des Autors des neuen Buches
+   * @param titel      Titel des neuen Buches
+   * @param hersteller Hersteller des neuen Buches
+   * @param bestand    Anzahl des Buches im Lager
+   * @return neue Buch-Instanz
+   * @throws RuntimeException wenn das Aktualisieren des Bestandes fehlschlägt
+   */
+  static public Buch neuesBuch(String autor, String titel,
+                               String hersteller, int bestand) {
+    Buch neuesBuch = new Buch();
+    neuesBuch.setAutor(autor);
+    neuesBuch.setTitel(titel);
+    neuesBuch.setHersteller(hersteller);
+    return neuesBuch;
+  }
 }
