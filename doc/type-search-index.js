9ff64ec7c63bc664ec90c83f1c62198e2161d2cd
Marcel Pohland <m.pohland@mwaysolutions.com>
2023-03-18 21:37:39 +0100

    generate javadocs

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"onlineshop.waren","l":"Artikel"},{"p":"onlineshop.waren","l":"Buch"},{"p":"onlineshop","l":"Main"},{"p":"onlineshop","l":"OnlineShop"},{"p":"onlineshop.waren","l":"Sachbuch"}];updateSearchResults();
