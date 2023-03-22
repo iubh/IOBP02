d806054d79ad692983e7889b3c9be01e3c4ac186
Marcel Pohland <m.pohland@mwaysolutions.com>
2023-03-22 21:27:50 +0100

    use logger

--- src/onlineshop/OnlineShop.java
+++ src/onlineshop/OnlineShop.java
@@ -1,7 +1,20 @@
 package onlineshop;
 
+import org.apache.logging.log4j.Logger;
+
+import onlineshop.waren.Buch;
+
+import org.apache.logging.log4j.LogManager;
+
 /**
  * The Class OnlineShop.
  */
 public class OnlineShop {
+	private static Logger logger = LogManager.getRootLogger();
+	public static void main(String[] args){
+		Buch buch = new Buch();
+		buch.setAnzahlSeiten(42);
+		buch.setAutor("meister");
+		logger.info(buch);
+	}
 }
--- src/onlineshop/waren/Sachbuch.java
+++ src/onlineshop/waren/Sachbuch.java
@@ -6,7 +6,7 @@ package onlineshop.waren;
 public class Sachbuch extends Buch {
 
     /** The Constant KLASSEN_PRAEFIX. */
-    private static final String KLASSEN_PRAEFIX = "SB";
+    //private static final String KLASSEN_PRAEFIX = "SB";
 
     /** The thema. */
     private String thema;
