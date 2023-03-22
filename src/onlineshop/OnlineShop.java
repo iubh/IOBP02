e26a999e8cecd8023e9c610f2e66b56fa894475e
Marcel Pohland <m.pohland@mwaysolutions.com>
2023-03-22 21:52:59 +0100

    add scanner

--- src/onlineshop/OnlineShop.java
+++ src/onlineshop/OnlineShop.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.Logger;
 
 import onlineshop.waren.Buch;
 
+import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 
 // TODO: Auto-generated Javadoc
@@ -11,19 +13,37 @@ import org.apache.logging.log4j.LogManager;
  * The Class OnlineShop.
  */
 public class OnlineShop {
-	
+
 	/** The logger. */
 	private static Logger logger = LogManager.getRootLogger();
-	
+
 	/**
 	 * The main method.
 	 *
 	 * @param args the arguments
 	 */
-	public static void main(String[] args){
-		Buch buch = new Buch();
-		buch.setAnzahlSeiten(42);
-		buch.setAutor("meister");
-		logger.info(buch);
+	public static void main(String[] args) {
+
+		System.out.println("Bitte eine Zahl auswählen (1 oder2): ");
+		Scanner s = new Scanner(System.in);
+		int eingabe = s.nextInt();
+		switch (eingabe) {
+			case 1:
+				Buch buch = new Buch();
+				buch.setAnzahlSeiten(42);
+				buch.setAutor("meister");
+				logger.info(buch);
+				logger.debug("1");
+				s.close();
+				break;
+			case 2:
+				Buch buch2 = new Buch();
+				buch2.setAnzahlSeiten(24);
+				buch2.setAutor("gandalf");
+				logger.info(buch2);
+				logger.debug("2");
+				s.close();
+				break;
+		}
 	}
 }
