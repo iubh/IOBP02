b724456b736e61366b53efd5a781ee55aede9fe0
Marcel Pohland <m.pohland@mwaysolutions.com>
2023-03-22 21:28:21 +0100

    docs

--- src/onlineshop/OnlineShop.java
+++ src/onlineshop/OnlineShop.java
@@ -6,11 +6,20 @@ import onlineshop.waren.Buch;
 
 import org.apache.logging.log4j.LogManager;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class OnlineShop.
  */
 public class OnlineShop {
+	
+	/** The logger. */
 	private static Logger logger = LogManager.getRootLogger();
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args){
 		Buch buch = new Buch();
 		buch.setAnzahlSeiten(42);
