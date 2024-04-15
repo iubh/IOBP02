183688ad5bc743b7f4526f5ecba011718cbee536
artingo <alfred.walther@artingo.net>
2024-04-15 13:14:46 +0200

    Einheit 3: Log4j2 hinzugefügt. Testfall-Eingabe hinzugefügt.

--- src/onlineshop/OnlineShop.java
+++ src/onlineshop/OnlineShop.java
@@ -1,8 +1,17 @@
 package onlineshop;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Managed den OnlineShop.<br/>
  * Hier wird später die Spring Boot Applikation gestartet.
  */
 public class OnlineShop {
+    private static Logger logger = LogManager.getLogger(OnlineShop.class);
+
+    public static void main(String[] args) {
+        Kunde kunde = new Kunde( "Karl", "Rietmann", Kunde.MAENNLICH, "27.05.1986", new Warenkorb());
+        logger.info(kunde);
+    }
 }
